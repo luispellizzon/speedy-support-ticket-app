fdc47e0c69c42b32d566f6948fbd108e2ee85cfe
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-16 17:11:16 +0100

    Setting up express server and PORT on .env file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server running");
+const { response } = require("express");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Speedy Support Ticket API" });
+});
+
+app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
