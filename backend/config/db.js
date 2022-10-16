d6081914911126da710d0db231771bd70dd233e1
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-16 19:00:31 +0100

    Connected to Database and user schema created to create users using their email, name and password.If user is created, it will send a json obj with the infos, and create on the database cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 /* @desc   Register new user */
 /* @route  /api/users */
@@ -10,6 +12,36 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please include all fields");
+  }
+
+  //Find if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user");
   }
 });
 
