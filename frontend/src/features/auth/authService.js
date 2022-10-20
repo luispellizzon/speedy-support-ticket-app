db96bdc96793c0a9771dbd3e8a28d095c099bdd9
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-20 15:14:34 +0100

    Logout functionallity working

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,8 +13,13 @@ const register = async (userData) => {
   return response.data;
 };
 
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
 const authService = {
   register,
+  logout,
 };
 
 export default authService;
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -38,6 +38,9 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
   console.log(user);
 });
 
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -63,6 +66,9 @@ export const authSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null;
       });
   },
