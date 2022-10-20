d80e6664f68d1fc20fb6e0e112a3d1cc78441fd6
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-20 16:13:40 +0100

    Working on validade user login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,12 +13,22 @@ const register = async (userData) => {
   return response.data;
 };
 
+const login = async (userLogin) => {
+  const response = await axios.post(`${API_URL}/login`, userLogin);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 const logout = () => {
   localStorage.removeItem("user");
 };
 
 const authService = {
   register,
+  login,
   logout,
 };
 
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -35,12 +35,22 @@ export const register = createAsyncThunk(
 
 /* -- Login user -- */
 export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
-  console.log(user);
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
 });
 
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
