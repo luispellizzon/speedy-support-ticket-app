ba4e1e1cf68f3f3a38d7e162e59b6fceaec57170
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-22 17:58:07 +0100

    Start working on single ticket page"

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -3,19 +3,37 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getTicket, reset } from "../features/ticket/ticketSlice";
 import { toast } from "react-toastify";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 function Ticket() {
 	const dispatch = useDispatch();
 	const params = useParams();
-	const { ticket, isError, isSuccess } = useSelector((state) => state.ticket);
+	const { ticket, isError, message, isLoading } = useSelector((state) => state.ticket);
 
 	useEffect(() => {
+		if (isError) {
+			toast.error(message);
+		}
 		dispatch(getTicket(params.ticketId));
-	}, [dispatch]);
+		dispatch(reset());
+	}, [dispatch, params.ticketId]);
 
+	if (isLoading) {
+		return <Spinner />;
+	}
+	if (isError) {
+		return <h2>Sorry, but something went wrong. Our team is working on the problem!</h2>;
+	}
 	return (
-		<div className="div">
-			<h1>Ticket {ticket.description}</h1>
+		<div className="ticket-page">
+			<header className="ticket-header">
+				<BackButton url="/tickets" />
+				<h2>
+					Ticket ID: {ticket._id}
+					<span className={`status status-${ticket.status}`}>{ticket.status}</span>
+				</h2>
+			</header>
 		</div>
 	);
 }
