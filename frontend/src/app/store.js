2545b95604e920a539940e0a4ab95b42e05f4d26
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-23 14:57:41 +0100

    Create notes Slice and Service on Redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import ticketReducer from "../features/ticket/ticketSlice";
+import notesReducer from "../features/notes/notesSlice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		ticket: ticketReducer,
+		notes: notesReducer,
 	},
 });
---
+++ frontend/src/features/notes/notesService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const API_URL = "/:ticketId/notes";
+
+/* -- Get Notes -- */
+const getNotes = () => {};
+
+const notesService = {
+	getNotes,
+};
+
+export default notesService;
