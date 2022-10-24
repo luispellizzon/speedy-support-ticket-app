ae1384e404c5816ec1eebc3f6bb8dbed9286dcc7
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-24 12:23:59 +0100

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 	app.get("*", (_, res) => {
-		res.sendFile(__dirname, "../frontend/build/index.html");
+		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 	});
 } else {
 	app.get("/", (_, res) => {
