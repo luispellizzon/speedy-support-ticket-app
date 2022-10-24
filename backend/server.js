d2f43ae4b3a235077363fc2c461386579c29b48f
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-24 12:46:31 +0100

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.status(200).json({ message: "Welcome to Speedy Support Ticket API" });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
@@ -34,12 +30,12 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 	});
 } else {
-	// app.get("/", (_, res) => {
-	// 	res.status(200).json({ message: "Welcome to the Support Desk API" });
-	// });
-	app.get("*", (_, res) => {
-		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+	app.get("/", (_, res) => {
+		res.status(200).json({ message: "Welcome to the Support Desk API" });
 	});
+	// app.get("*", (_, res) => {
+	// 	res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+	// });
 }
 
 app.use(errorHandler);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,12 +19,15 @@ function App() {
 					<Header />
 					<Routes>
 						<Route path="/" element={<Home />} />
+
 						<Route path="/login" element={<Login />} />
+
 						<Route path="/register" element={<Register />} />
 
 						<Route path="/tickets" element={<PrivateRoute />}>
 							<Route path="/tickets" element={<Tickets />} />
 						</Route>
+
 						<Route path="/ticket/:ticketId" element={<PrivateRoute />}>
 							<Route path="/ticket/:ticketId" element={<Ticket />} />
 						</Route>
