e52abff952efdfde98227b02e2eb7df43991f3c0
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-24 12:29:26 +0100

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,12 @@ if (process.env.NODE_ENV === "production") {
 	app.get("*", (_, res) => {
 		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 	});
-} else {
-	app.get("/", (_, res) => {
-		res.status(200).json({ message: "Welcome to Speedy Support Ticket API" });
-	});
-}
+// } else {
+//   app.get("/", (_, res) => {
+    
+// 		res.status(200).json({ message: "Welcome to Speedy Support Ticket API" });
+// 	});
+// }
 
 app.use(errorHandler);
 
