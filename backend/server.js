c2e1876e93d7fa89c095cb20a4767639497c9f4e
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-24 12:37:40 +0100

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -26,18 +26,18 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 // Serve frontend FOR PRODUCTION to deploy on Heroku
 if (process.env.NODE_ENV === "production") {
-	//Set build folder as static
+	// Set build folder as static
 	app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+	// FIX: below code fixes app crashing on refresh in deployment
 	app.get("*", (_, res) => {
 		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 	});
-// } else {
-//   app.get("/", (_, res) => {
-    
-// 		res.status(200).json({ message: "Welcome to Speedy Support Ticket API" });
-// 	});
-// }
+} else {
+	app.get("/", (_, res) => {
+		res.status(200).json({ message: "Welcome to the Support Desk API" });
+	});
+}
 
 app.use(errorHandler);
 
